671c634e52a87eaa32950392e33fab9fa14f0764
Xackery Xtal <xackery@gmail.com>
2015-11-25 03:01:46 -0800

    Created comments, split the instruction echos to partition steps, and uncommented wget.

--- go.sh
+++ go.sh
@@ -2,20 +2,23 @@
 
 echo "Getting Go..."
 FILE="go1.5.1.linux-386.tar.gz..."
-#64 bit
 if [ `uname -m | grep 64 | wc -l` -ge 1 ]; then  
-	FILE="go1.5.1.linux-amd64.tar.gz"
+	FILE="go1.5.1.linux-amd64.tar.gz" #Get 64 bit version
 fi
+
 echo "Downloading $FILE"
-#wget https://storage.googleapis.com/golang/$FILE
+wget https://storage.googleapis.com/golang/$FILE
 if [ ! -f $FILE ]; then
     echo "File failed to download"
     exit
 fi
+
 echo "Extracting $FILE to /usr/local/go..."
 tar -C /usr/local -xzf $FILE
+
 echo "Removing $FILE"
 rm $FILE
+
 echo "Making a go path in home directory"
 mkdir ~/go
 echo "Last Steps: You need to copy the below line and add to ~/.bashrc, likely bottom, or append to previous lines"
